778a7d63386ab783dcb9790ec8a3dd6b86537c7c
Uyen-nt-Le <47649760+Uyen-nt-Le@users.noreply.github.com>
2020-01-28 15:03:27 -0800

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,16 @@
+/*
+ * Uyen Le 		11-14-2019
+ * Final Project: Tetris
+ * This program displays the Tetris game.
+ */
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TetrisGame game = new TetrisGame();
+		game.setVisible(true);
+
+		game.update();
+	}
+	
+}
